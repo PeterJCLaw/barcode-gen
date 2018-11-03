bb9e392be237bb361e7de50c65f2e87add41247d
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-11-03 19:36:05 +0100

    use QR code and SR barcode

--- barcode-gen.py
+++ barcode-gen.py
@@ -2,27 +2,22 @@
 import time
 import argparse
 import re
-
-import code128
+import pyqrcode
+import io
 
 parser = argparse.ArgumentParser()
 parser.add_argument('codes', metavar='NNNNNNNN', nargs='+', help="list of codes to generate (with or without dashes)")
 args = parser.parse_args()
 
 # sanitise codes, so we can support both with and without dashes.
-codes = [re.sub(r'[^0-9]','',code) for code in args.codes]
-dashed_codes = ["-".join([code[0:2],code[2:4],code[4:6],code[6:8]]) for code in codes]
-
-for code in codes:
-    if len(code) != 8:
-        raise Exception("Invalid code length: {}, should be 8 digits long".format(code))
-
+codes =  args.codes
+# translate(-10,0) scale(0.7, 1.2)
 template = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE svg
   PUBLIC '-//W3C//DTD SVG 1.1//EN'
   'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
 <svg height="29.000mm" version="1.1" width="90.300mm" xmlns="http://www.w3.org/2000/svg">
-<g transform="translate(-10,0) scale(0.7, 1.2)">
+<g transform="translate(-2,-2)">
 {bar}
 </g>
 <g>
@@ -35,10 +30,12 @@ style="fill:black;font-size:16pt;text-anchor:middle;"
 </text>
 </g>
 </svg>"""
-for (dashed_code, code) in zip(dashed_codes, codes):
+for code in codes:
     out_file = code+".svg"
-    svg = code128.svg(code)
-
+    qr = pyqrcode.create(code)
+    svg = io.BytesIO()
+    qr.svg(svg, scale=4)
+    svg_content = svg.getvalue().decode("utf-8")
+    svg_content = '\n'.join(svg_content.split('\n')[1:])
     with open(out_file,'w') as o:
-        svg_content = "\n".join(svg.split("\n")[3:-2])
-        print(template.format(bar=svg_content,code=dashed_code),file=o)
+        print(template.format(bar=svg_content,code=code),file=o)
