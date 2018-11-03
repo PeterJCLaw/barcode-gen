ca9116372b1c151f7fde074f34456d8783fc875a
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-11-03 23:28:11 +0100

    remove the logo from the short form kits, add the qr code

--- barcode-gen.py
+++ barcode-gen.py
@@ -290,16 +290,16 @@ templates = {
      borderopacity="1.0"
      inkscape:pageopacity="0.0"
      inkscape:pageshadow="2"
-     inkscape:zoom="4.5993104"
-     inkscape:cx="117.13058"
-     inkscape:cy="66.953128"
+     inkscape:zoom="3.8365839"
+     inkscape:cx="212.80249"
+     inkscape:cy="66.28275"
      inkscape:document-units="mm"
      inkscape:current-layer="g1046"
      showgrid="true"
-     inkscape:window-width="1244"
-     inkscape:window-height="1313"
-     inkscape:window-x="1312"
-     inkscape:window-y="86"
+     inkscape:window-width="2560"
+     inkscape:window-height="1440"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
      inkscape:window-maximized="0"
      showguides="false"
      inkscape:snap-text-baseline="true">
@@ -330,6 +330,9 @@ templates = {
       </cc:Work>
     </rdf:RDF>
   </metadata>
+ <g transform="translate(3,1.8) scale(0.16,0.16)">
+    {bar}
+ </g>
   <g
      inkscape:label="Layer 1"
      inkscape:groupmode="layer"
@@ -377,17 +380,6 @@ templates = {
            y="43.744675"
            style="font-size:10.66668034px;line-height:69.99999881%;stroke-width:1.00000131px"
            id="tspan5576">Do not remove or cover this label</tspan></text>
-      <g
-         style="fill:none;fill-rule:evenodd;stroke:none;stroke-width:1"
-         id="Symbol"
-         transform="matrix(0.07828672,0,0,0.07828672,-4.3442337,-38.61822)">
-        <path
-           transform="translate(-7,0)"
-           style="fill:#000000;fill-rule:nonzero"
-           inkscape:connector-curvature="0"
-           d="M 677.02569,617.02111 C 647.86936,653.56793 611.78587,684.96457 570,709.08965 c -117.57241,67.88047 -262.42759,67.88047 -380,0 C 149.36918,685.63144 114.12985,655.29811 85.407993,620.03996 237.45689,462.35413 363.12698,500.77008 363.12698,444.72918 c 0,-58.40687 -216.99267,-96.85099 -216.99267,-137.37364 0,-40.41177 127.88133,-71.09181 236.54807,-71.25942 108.66671,0.16761 236.54802,30.84765 236.54802,71.25942 0,40.52265 -216.99268,78.96677 -216.99268,137.37364 0,55.6804 124.05847,18.11529 274.78795,172.29195 z m 31.57148,-46.17064 C 627.79949,503.08549 552.57476,461.04506 482.92298,444.72918 c 34.56671,-27.89165 229.03772,-98.84427 229.03772,-136.825 0,-37.84011 -167.95212,-128.40681 -328.38423,-129.07691 -0.298,-0.004 -0.59603,-0.003 -0.89409,-0.003 -0.2981,-6.2e-4 -0.59613,-9.3e-4 -0.89415,-9.3e-4 -160.4321,0.67384 -328.384228,91.24054 -328.384228,129.08065 0,37.98073 194.471018,108.93335 229.037718,136.825 C 211.75955,461.28643 135.33833,504.33496 53.178046,573.87479 18.942376,516.16059 -1.4631037e-6,449.42886 0,380 -2.8609475e-6,244.23908 72.427587,118.79081 190,50.910347 c 117.57241,-67.880463 262.42759,-67.880463 380,0 C 687.57241,118.79081 760,244.23908 760,380 c 0,68.2194 -18.28817,133.83483 -51.40283,190.85047 z"
-           id="Combined-Shape" />
-      </g>
       <path
          style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:1.00000131px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
          d="M 201.29501,-47.045993 V 47.442318"
